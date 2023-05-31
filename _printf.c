927be78612f08c80745abb2e4a9cc2ac9496378c
Liz-Muchiri <leezmuchiri@gmail.com>
2023-05-30 18:57:20 -0700

    My files

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdarg.h>
 
 /**
@@ -9,41 +10,42 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i, count = 0;
+    va_list args;
+    int i, count = 0;
+    char buffer[1024];
 
-va_start(args, format);
+    va_start(args, format);
 
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
-{
-case 'c':
-count += print_char(args, NULL, va_arg(args, int));
-break;
-case 's':
-count += print_string(args, NULL, va_arg(args, char *));
-break;
-case '%':
-count += print_percent(args, NULL);
-break;
-default:
-count += print_percent(args, NULL);
-count += print_char(args, NULL, format[i]);
-break;
-}
-}
-else
-{
-count += print_char(args, NULL, format[i]);
-}
-}
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    count += handle_print("%c", args, buffer, 0, 0, 0, 0);
+                    break;
+                case 's':
+                    count += print_string(args, buffer, 0, 0, 0, 0, va_args(args, char*));
+                    break;
+                case '%':
+                    count += print_percent(args, buffer, 0, 0, 0, 0);
+                    break;
+                default:
+                    count += print_percent(args, buffer, 0, 0, 0, 0);
+                    count += print_char(args, buffer, 0, 0, 0, 0, format[i]);
+                    break;
+            }
+        }
+        else
+        {
+            count += print_char(args, buffer, format[i]);
+        }
+    }
 
-va_end(args);
+    va_end(args);
 
-return (count);
+    return (count);
 }
 
