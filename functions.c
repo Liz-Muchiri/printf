225d66e70b86c8316c8be514bf765db78804274e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-30 19:48:05 -0700

    formats d and i

---
+++ functions.c
@@ -0,0 +1,49 @@
+#include "m2.h"
+/**
+ * _printf - prints acording to a format
+ * @format: character string
+ * Return: count
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	int i = 0;
+
+	va_list args;
+
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			format++;
+			if (*format == 'd' || *format == 'i')
+			{
+
+				count++;
+				write(1, &format[i], i);
+
+			}
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			write(1, &format[i], i);
+			count++;
+		}
+		else
+		{
+			write(1, &format[i], i);
+			count++;
+		}
+		format++;
+		i++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
