df04b3de3ec1f19710f271d9e6d57eb352ff8bf6
Vicky-E <victoriaemmanuel630@gmail.com>
2023-06-02 17:13:42 -0700

    prints decimals and integers

---
+++ print_numbers.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "m2.h"
+/**
+ * _printf - prints specifiers d and i
+ * @format: specifier string
+ * Return: count
+ */
+int _putchar( char c);
+int _printf(const char *format, ...)
+{
+	int i, count;
+	unsigned long int num;
+	va_list numbers;
+
+	va_start(numbers, format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				num = va_arg(numbers, int);
+				if (num < 0)
+				{
+					_putchar(num * (-1));
+					count++;
+				}
+				else if (num > 99)
+				{
+					_putchar((num / 100) + '0');
+					_putchar(((num / 10) % 10) + '0');
+					_putchar((num % 10) + '0');
+					count++;
+				}
+				else if (num > 9)
+				{
+					_putchar((num / 10) + '0');
+					_putchar((num % 10) + '0');
+					count++;
+				}
+				else
+				{
+					_putchar((num % 10) + '0');
+					count++;
+				}
+			}
+			else
+			{
+				write(1, &format[i], 1);
+				count++;
+			}
+		}
+	}
+	return (count);
+}
+/**
+ * _putchar - prints a character
+ * @a: character to be peinted
+ * Return: integer
+ */
+int _putchar(char a)
+{
+	return(write(1, &a, 1);
+}
